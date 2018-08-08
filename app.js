8de16e5d017a919ecec1263b3a8761b3ecd0d997
Prashanth Molakala <pmolakal@hawk.iit.edu>
2018-08-08 00:54:50 -0500

    Update app.js

--- app.js
+++ app.js
@@ -37,14 +37,14 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 
-app.use(express.static(path.join(__dirname, 'public')));
-
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   res.header("Access-Control-Allow-Methods", "GET, POST, PUT, OPTIONS");
   next();
 });
+
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
 app.use('/dishes',dishRouter);
